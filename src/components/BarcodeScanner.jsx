1774fc206353f380ba34ca30973a06ace326c1b5
reddirani8496 <reddiranibattuluri@gmail.com>
2025-05-01 15:46:14 +0530

    Api call updated

--- src/components/BarcodeScanner.jsx
+++ src/components/BarcodeScanner.jsx
@@ -500,12 +500,8 @@ export default function BarcodeScanner() {
           const code = result.getText();
           try {
             const response = await productApi.getByCode(code);
-            console.log("inside try block response");
-            alert("resp ", response);
             if (response.data) {
               const cartResponse = await cartApi.addToCart("user123", code);
-              console.log("cart response ", cartResponse);
-              alert("cart response", cartResponse);
               setCart(cartResponse.data);
               toast.success("Product added to cart!");
             }
@@ -577,6 +573,7 @@ export default function BarcodeScanner() {
 
   // Generate PDF bill
   const generatePDF = () => {
+    console.log("inside generate pdf");
     const doc = new jsPDF();
     doc.text("Shopping Bill", 14, 16);
     doc.autoTable({
